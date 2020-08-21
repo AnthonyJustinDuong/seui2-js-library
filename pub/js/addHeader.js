a69e72098df8a5724e27747512beeb21bbc01faf
Anthony Justin Duong <anthony.duong@mail.utoronto.ca>
2020-08-21 11:14:43 -0400

    Added spec options table to documentation.

--- pub/js/addHeader.js
+++ pub/js/addHeader.js
@@ -10,7 +10,7 @@
         </nav>
 
         <div class="logo">
-          <a href="/"><img src="images/seui2-logo3.png" height="21px"></a>
+          <a href="/"><img src="images/seui2-logo2.png" height="21px"></a>
         </div>`
   const oldHeader = document.body.querySelector('#main-header');
   document.body.replaceChild(newHeader, oldHeader);
